31f6c769f62c92bd8a3437110d0daa72fdf755e3
Reynan Paiva <reynanwq@gmail.com>
2024-03-14 12:48:13 -0300

    Generate Spring Initializr

---
+++ demoday/src/test/java/com/demodayapi/DemodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demodayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
