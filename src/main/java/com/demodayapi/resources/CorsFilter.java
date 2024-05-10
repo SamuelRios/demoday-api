917f8ba3a88900dc3c6c09abf2f625948c422760
Samuel Rios <mucarioss@gmail.com>
2024-05-09 22:48:35 -0300

    save localhost domain

--- src/main/java/com/demodayapi/resources/CorsFilter.java
+++ src/main/java/com/demodayapi/resources/CorsFilter.java
@@ -11,7 +11,7 @@ public class CorsFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "https://demoday-omega.vercel.app");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000, https://demoday-omega.vercel.app");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Allow-Credentials", "true");
