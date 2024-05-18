cfe8c527d66dbd1a6c4b21539840d61f5a58c27f
Samuel Rios <mucarioss@gmail.com>
2024-05-18 13:50:51 -0300

    set localhost origing

--- src/main/java/com/demodayapi/resources/CorsFilter.java
+++ src/main/java/com/demodayapi/resources/CorsFilter.java
@@ -6,12 +6,24 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 @Component
 public class CorsFilter extends OncePerRequestFilter {
 
+    private static final List<String> ALLOWED_ORIGINS = Arrays.asList(
+        "https://demoday-omega.vercel.app",
+        "https://localhost:3000"
+    );
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "https://demoday-omega.vercel.app");
+        String origin = request.getHeader("Origin");
+        System.out.println("ORIGIN:::::::::::::::::::::::");
+        System.out.println(origin);
+        if (ALLOWED_ORIGINS.contains(origin)) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+        }
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
         response.setHeader("Access-Control-Allow-Credentials", "true");
