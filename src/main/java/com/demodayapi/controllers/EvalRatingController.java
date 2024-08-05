a4e7d911292d0509ba42cf094d80c2313f4443c1
Samuel Rios <mucarioss@gmail.com>
2024-08-05 01:01:45 -0300

    save

--- src/main/java/com/demodayapi/controllers/EvalRatingController.java
+++ src/main/java/com/demodayapi/controllers/EvalRatingController.java
@@ -30,9 +30,27 @@ public class EvalRatingController {
     private DemodayService demodayService;
 
     @GetMapping("/totalRatings")
-    public ResponseEntity<Map<Integer, Double>> getAllRatings() {
+    public ResponseEntity<?> getAllRatings(@RequestParam(defaultValue = "phase") Integer phase, @RequestParam(defaultValue = "demodayId") Integer demodayId ) {
         try {
-            Map<Integer, Double> allRatings = evalRatingService.getAllProjectRatings();
+            System.out.println(phase);
+            System.out.println("aaaalooo");
+
+            Map<Integer, Double> allRatings;
+            
+            if(phase == 3){
+                
+                if(this.demodayService.isAfterDemodayPhase(demodayId, DemodayStatusEnum.PHASE3)){
+                    allRatings = evalRatingService.getProjectRatingsByDemodayAndPhase(demodayId, PhaseEvalRateEnum.PHASE3);
+                } else return new ResponseEntity<>("Fase 3 ainda não foi finalizada.", HttpStatus.UNAUTHORIZED);
+                
+            } else if(phase == 4){
+
+                if(this.demodayService.isAfterDemodayPhase(demodayId, DemodayStatusEnum.PHASE4)){
+                    allRatings = evalRatingService.getProjectRatingsByDemodayAndPhase(demodayId, PhaseEvalRateEnum.PHASE4);
+                } else return new ResponseEntity<>("Fase 4 ainda não foi finalizada.", HttpStatus.UNAUTHORIZED);
+
+            } else return new ResponseEntity<>("Só é possível calcular as notas para a fase 3 ou para fase 4.", HttpStatus.BAD_REQUEST);
+
             Map<Integer, Double> decimalRatings = allRatings.entrySet().stream()
                     .collect(Collectors.toMap(
                             Map.Entry::getKey,
@@ -42,36 +60,6 @@ public class EvalRatingController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
-
-
-    @GetMapping("/totalRating")
-    public ResponseEntity<?> getAllRatings(@RequestParam(defaultValue = "phase") Integer phase, @RequestParam(defaultValue = "demodayId") Integer demodayId ) {
-        System.out.println(phase);
-        System.out.println("aaaalooo");
-
-        Map<Integer, Double> allRatings;
-        
-        if(phase == 3){
-            
-            if(this.demodayService.isAfterDemodayPhase(demodayId, DemodayStatusEnum.PHASE3)){
-                allRatings = evalRatingService.getProjectRatingsByDemodayAndPhase(demodayId, PhaseEvalRateEnum.PHASE3);
-            } else return new ResponseEntity<>("Fase 3 ainda não foi finalizada.", HttpStatus.UNAUTHORIZED);
-            
-        } else if(phase == 4){
-
-            if(this.demodayService.isAfterDemodayPhase(demodayId, DemodayStatusEnum.PHASE4)){
-                allRatings = evalRatingService.getProjectRatingsByDemodayAndPhase(demodayId, PhaseEvalRateEnum.PHASE4);
-            } else return new ResponseEntity<>("Fase 4 ainda não foi finalizada.", HttpStatus.UNAUTHORIZED);
-
-        } else return new ResponseEntity<>("Só é possível calcular as notas para a fase 3 ou para fase 4.", HttpStatus.BAD_REQUEST);
-
-        Map<Integer, Double> decimalRatings = allRatings.entrySet().stream()
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        entry -> Math.round(entry.getValue() * 100.0) / 100.0,
-                        (e1, e2) -> e1, LinkedHashMap::new));
-        return new ResponseEntity<>(decimalRatings, HttpStatus.OK);
     }
 
 
